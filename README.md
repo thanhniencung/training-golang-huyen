fd9a94ecd8dd3c9a1d6ce990988e302c2fb3fab1
Ryan <ryan.vinova@gmail.com>
2024-07-05 21:42:19 +0700

    demo go build

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # training-golang-huyen
+
+## Build app
+```
+1. GOOS=linux go build -o app-for-linux variable/main.go
+2. GOOS=darwin go build -o app-for-mac variable/main.go
+```
---
+++ variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var email string = "ryan@gmail.com"
+	fmt.Println(email)
+
+	fullName := "Ryan"
+	fmt.Println(fullName)
+
+	var phone = "0974901734"
+	fmt.Println(phone)
+}
